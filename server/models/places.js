34c7b02e278e5441c7ba28619722e8321f82f73c
Karthik Bhat A <karthik.bhata7@gmail.com>
2017-03-25 17:17:46 +0530

    Place schema defined

---
+++ server/models/places.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var placeSchema = {
+  name: String,
+  latitude: Number,
+  longitude: Number,
+  rating: Number,
+  numberOfRating: Number,
+  description: String
+  comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
+}
+
+module.exports.places = mongoose.model('Place', placeSchema);
