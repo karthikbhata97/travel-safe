70a0c51bd8c49917008ef0daeaff1a5419af22c3
Karthik Bhat A <karthik.bhata7@gmail.com>
2017-03-25 18:02:37 +0530

    Skeleton

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express'),
     app = express(),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/test');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(morgan('dev'));
+
+app.use('/client', express.static(__dirname + '/client'));
+app.use(express.static(__dirname + 'public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/views/index.html');
+});
+
 
 app.listen(3000, function() {
   console.log("Hello 3k");
